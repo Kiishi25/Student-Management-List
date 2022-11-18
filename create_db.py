bf6e01c56f6177d205485e3412594e00d4926000
Kiishi25 <55563179+Kiishi25@users.noreply.github.com>
2022-11-18 09:29:33 +0000

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
-mydb = mysql.connector.connect(host='localhost',user='root',passwd= 'O21H27BabaM!&',)
+mydb = mysql.connector.connect(host='localhost',user='root',passwd= '',)
 
 my_cursor = mydb.cursor()
 #my_cursor.execute('CREATE DATABASE student_list')
